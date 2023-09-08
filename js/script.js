7d826b2ab41f7da0b3b370d43c91d2c376e2f238
EmanueleDiLeo <dileoemanuele11@gmail.com>
2023-09-08 16:44:28 +0200

    [ADD]: form js

--- js/script.js
+++ js/script.js
@@ -1,10 +1,17 @@
-const name = document.getElementById("name");
-const kilometres = document.getElementById("km");
-const age = document.getElementById("age");
-let check = true;
-let message;
+function validateForm() {
+  const name = document.forms["my-form"]["name"].value;
+  const kilometres = parseInt(document.forms["my-form"]["km"].value);
+  const age = parseInt(document.forms["my-form"]["km"].value);
+  let check = true;
+  let message;
 
-if(isNaN(age) || isNaN(kilometres) || age < 0 || kilometres < 0){
-  message = "Valori inseriti non validi";
-  check = false;
+  if(isNaN(kilometres) || kilometres < 0){
+    message = "Valore di kilometri non valido";
+    check = false;
+  }
+  console.log(age);
+
+  document.getElementById("message").innerHTML = message;
+  
 }
+
