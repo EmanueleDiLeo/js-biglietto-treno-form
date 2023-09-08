f9cb2f8837795aebb8bdde30d89d1ff0b10e16ac
EmanueleDiLeo <dileoemanuele11@gmail.com>
2023-09-08 23:05:26 +0200

    [ADD] : bottons js

--- js/script.js
+++ js/script.js
@@ -1,37 +1,55 @@
-const buttonCreate = document.getElementById("create")
+const buttonCreate = document.getElementById("create");
+const buttonDelete = document.getElementById("delete");
 
 buttonCreate.addEventListener("click", function(){
-
-  let name = document.getElementById("name").value;
-  let kilometres = parseInt(document.getElementById("km")).value;
-  let age = document.getElementById("age").value;
+  const name = document.getElementById("name").value;
+  const kilometres = parseFloat(document.getElementById("km").value);
+  const age = document.getElementById("age").value;
   let check = true;
-  let message;
+
+  text.style.display = "block";
+  
   let price;
+  let offer = "Biglietto Standard";
+  let carriage = parseInt(Math.random()  * 10);
+  let code = parseInt(Math.random() * 100000);
 
   if(isNaN(kilometres) || kilometres < 0){
     message = "Valore di kilometri non valido";
     check = false;
+    ticket.style.display = "none";
   }
 
   if(check){
     price = kilometres * 0.21;
 
-    if(age === "Minorenne"){
-      price*= 0.8;
+    if(age === "minorenne"){
+      price *= 0.8;
+      offer = "Biglietto Minorenne";
     }
 
-    if(age === "Over 65"){
-      price*= 0.8;
+    if(age === "over 65"){
+      price *= 0.8;
+      offer = "Biglietto Over 65";
     }
 
     message = "IL TUO BIGLIETTO"
-    console.log(price);
+    ticket.style.display = "block";
   }
 
+  console.log(message);
+
   document.getElementById("message").innerHTML = message;
+  document.getElementById("js-name").innerHTML = name;
+  document.getElementById("js-offer").innerHTML = offer;
+  document.getElementById("js-carriage").innerHTML = carriage;
+  document.getElementById("js-code").innerHTML = code;
+  document.getElementById("js-price").innerHTML = price;
 
 });
   
 
-
+buttonDelete.addEventListener("click", function(){
+  ticket.style.display = "none";
+  text.style.display = "none";
+});
